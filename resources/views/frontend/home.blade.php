c28acd12d9fe130fc4d289f5b5c9fcfbbd17dd66
anhnguyenthi <anhnguyenthi@admicro.vn>
2020-05-21 07:02:58 +0700

    wip2

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -69,7 +69,103 @@
     
 </section>
 
+<section class="pre-certificate">
+    <div class="certificate-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="head_title text-center fix">
+                        <h2 class="text-uppercase">Luyện thi chứng chỉ</h2>
+                    </div>
+                    <div class="certi-content-wrapper">
+                        <nav>
+                            <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                                <a class="nav-item nav-link active" id="nav-certi-tab" data-toggle="tab" href="#nav-certi" role="tab" aria-controls="nav-certi" aria-selected="true">Chứng chỉ phụ trợ bảo hiểm</a>
+                                <a class="nav-item nav-link" id="nav-agency-tab" data-toggle="tab" href="#nav-agency" role="tab" aria-controls="nav-agency" aria-selected="false">Chứng chỉ đại lí bảo hiểm</a>
+                                <a class="nav-item nav-link" id="nav-human-tab" data-toggle="tab" href="#nav-human" role="tab" aria-controls="nav-human" aria-selected="false">Chứng chỉ phi nhân thọ cơ bản</a>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class="pre-certi-ctn">
+                        <section id="tabs">
+                            <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+                                <div class="tab-pane fade show active" id="nav-certi" role="tabpanel" aria-labelledby="nav-certi-tab">
+                                    <div class="certi-ctn tab-ctn">
+                                        @for($k=1;$k<=8;$k++)
+                                        <div class="certi-item">
+                                            <img src="{{ url('/') }}/assets/images/item-demo.png?{{ config('custom.version') }}" alt="">
+                                            <div class="name-certi">
+                                                Loại hình 1
+                                            </div>
+                                            <div class="price-certi">
+                                                400.000 <span>vnđ</span>
+                                            </div>
+                                            <div class="des-certi">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                            </div>
+                                            <div class="btn-container">
+                                            <a href="" class="btn btn-default m-top-20">Mô tả đề thi</a>
+                                            <a href="" class="btn btn-default m-top-20">Vào thi</a>
+                                            </div>
+                                        </div>
+                                        @endfor
+                                       
+                                    </div>
+                                    <div class="watch-more-cnt">
+                                        <a href="javascript:void()">Xem thêm</a>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="nav-agency" role="tabpanel" aria-labelledby="nav-agency-tab">
+                                    <div class="agency-ctn tab-ctn">
+                                        
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="nav-human" role="tabpanel" aria-labelledby="nav-human-tab">
+                                    <div class="human-ctn tab-ctn">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
+<section class="reason">
+    <div class="reason-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="head_title text-center fix">
+                        <h2 class="text-uppercase">Luyện thi chứng chỉ</h2>
+                    </div>
+                    <div class="reason-ctn">
+                        @for($l=1;$l<=4;%l++)
+                        <div class="reason-item">
+                            <img src="{{ url('/') }}/assets/images/home/teacher.png?{{ config('custom.version') }}" alt="">
+                            <div class="item-content">
+                                <div class="reason-title">
+                                    <h5>Giáo viên chất lượng</h5>
+                                </div>
+                                <div class="item-description">
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, eligendi eius illo cumque nobis nihil debitis cum modi deserunt magnam repudiandae? Nihil aperiam odio illum iusto doloribus corporis expedita ipsum.
+                                    </p>
+                                </div>
+                                <div class="watch-more-cnt">
+                                    <a href="javascript:void()">Xem thêm</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @stop
 
 @section('footer')
