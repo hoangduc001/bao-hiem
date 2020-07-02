6412a454d404ece9ca6172b9a33453037b18cfb8
hoangduc <hoangduc191100@gmail.com>
2020-07-02 14:35:46 +0700

    convert blade admin okok

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
-<html dir="ltr" lang="en">
-
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link  href="{{asset('css/favicon.png')}}">
+    <link rel="icon"  href="{{asset('assets/images/favicon.png')}}">
     <title>Matrix Template - The Ultimate Multipurpose admin template</title>
     <link href="{{asset('css/float-chart.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/style.min.css')}}">
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="{{asset('js/html5shiv.js')}}"></script>
+    <script src="{{asset('js/respond.min.js')}}"></script>
 </head>
 
 <body>
